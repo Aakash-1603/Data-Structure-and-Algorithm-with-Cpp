5fdbefb87525ae760b555c5514594dfa947cdfa5
Aakash Asthana <aakashasthana6@gmail.com>
2025-07-02 00:18:10 +0530

    Pointers

--- 3. Pointer/Problems/swapnum.cpp
+++ 3. Pointer/Problems/swapnum.cpp
@@ -21,5 +21,4 @@ int main()
 
     // The Time Complexity of this code is O(1) as it performs a constant number of operations regardless of the input size.
     // The Space Complexity of this code is O(1) as it uses a fixed amount of space for the variables.
-    //  The code swaps two numbers using pointers and prints the values before and after swapping.
 }
