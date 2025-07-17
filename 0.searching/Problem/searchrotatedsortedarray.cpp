3865c250d44df1dfb7bac886ba82f7399277e22f
Aakash Asthana <aakashasthana6@gmail.com>
2025-07-17 20:08:05 +0530

    Searching

--- 0.searching/Problem/searchrotatedsortedarray.cpp
+++ 0.searching/Problem/searchrotatedsortedarray.cpp
@@ -1,50 +1,60 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-int main(){
+int main()
+{
 
     // problem statement:-- Search the element in the rotated sorted array.
 
-
-    //example:-- 
-    // originl array -> [0,1,2,3,4,5,6,7]
-    // rotated sorted array -> [3,4,5,6,7,0,1,2]
-
-
+    // example:--
+    //  originl array -> [0,1,2,3,4,5,6,7]
+    //  rotated sorted array -> [3,4,5,6,7,0,1,2]
 
     // Brute force approach -> linear search.
 
     // optimised approach -> binary search.
     int n;
-    cin>>n;
+    cin >> n;
     int arr[n];
-    for(int i=0;i<n;i++){
-        cin>>arr[i];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
     }
     int target;
-    cin>>target;
-    int start=0;
-    int end=n-1;
-    while(start<=end){
-        int mid=start+(end-start)/2;
-        if(arr[mid]==target){
+    cin >> target;
+    int start = 0;
+    int end = n - 1;
+    while (start <= end)
+    {
+        int mid = start + (end - start) / 2;
+        if (arr[mid] == target)
+        {
             return mid;
         }
-        if(arr[start]<=arr[mid]){
-            if(arr[start]<=target && target<=arr[mid]){
-                end=mid-1;
+        if (arr[start] <= arr[mid])
+        {
+            if (arr[start] <= target && target <= arr[mid])
+            {
+                end = mid - 1;
             }
-            else{
-                start=mid+1;
+            else
+            {
+                start = mid + 1;
             }
         }
-        else{
-            if(arr[mid]<=target && target<=arr[end]){
-                start=mid+1;
+        else
+        {
+            if (arr[mid] <= target && target <= arr[end])
+            {
+                start = mid + 1;
             }
-            else{
-                end=mid-1;
+            else
+            {
+                end = mid - 1;
             }
         }
     }
     return -1;
+
+    // Time complexity is O(logn).
+    // Space complexity is O(n).
 }
