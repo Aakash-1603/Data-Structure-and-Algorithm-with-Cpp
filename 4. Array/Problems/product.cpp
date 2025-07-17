ab41bf0676bf26f54f7daf6ab0018f1d59bfedd7
Aakash Asthana <aakashasthana6@gmail.com>
2025-07-17 18:40:02 +0530

    Array

--- 4. Array/Problems/product.cpp
+++ 4. Array/Problems/product.cpp
@@ -70,3 +70,65 @@ int main()
 
     // but this code fails at 0 value present in array.
 }
+
+int main()
+{
+
+    // optimal solution:---
+
+    int n;
+    cin >> n;
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+    vector<int> prefix;
+    prefix[0] = arr[0];
+    for (int i = 1; i < n; i++)
+    {
+        prefix[i] = prefix[i - 1] * arr[i];
+    }
+    vector<int> suffix;
+    suffix[n - 1] = arr[n - 1];
+    for (int i = n - 2; i >= 0; i--)
+    {
+        suffix[i] = suffix[i + 1] * arr[i];
+    }
+    for (int i = 0; i < prefix.size(); i++)
+    {
+        cout << prefix[i] << " ";
+    }
+    cout << endl;
+    for (int i = 0; i < suffix.size(); i++)
+    {
+        cout << suffix[i] << " ";
+    }
+    cout << endl;
+    vector<int> res;
+    for (int i = 0; i < n; i++)
+    {
+        if (i == 0)
+        {
+            res.push_back(suffix[i + 1]);
+        }
+        else if (i == n - 1)
+        {
+            res.push_back(prefix[n - 2]);
+        }
+        else
+        {
+            res.push_back(prefix[i - 1] * suffix[i + 1]);
+        }
+    }
+    for (int i = 0; i < res.size(); i++)
+    {
+        cout << res[i] << " ";
+    }
+    return 0;
+
+    // Time complexity is O(n).
+    // Space complexity is O(n).
+
+    // if you also want to remove the prefix and suffix array space from the code you can directly put the result of pultipliction into the res arry.
+}
